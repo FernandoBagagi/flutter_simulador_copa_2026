ff9150630f02645f958386e967497937bdf345dd
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-06-12 14:29:37 -0300

    ajeita tamanhos da AppBar da HomeScreen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -9,29 +9,23 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(
-              Icons.sports_soccer,
-              size: 80,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            title: Text(
-              'Simulador Copa do Mundo 2026',
-              style: Theme.of(
-                context,
-              ).textTheme
-              .headlineMedium
-              ?.copyWith(
-                fontWeight: FontWeight.bold
-                ),
-            ),
+          Icons.sports_soccer,
+          size: 56,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        title: Text(
+          'Simulador Copa do Mundo 2026',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           FilledButton.icon(
             onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => const SelecoesScreen()),
-              );
+              Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (_) => const SelecoesScreen()));
             },
             icon: const Icon(Icons.list),
             label: const Padding(
