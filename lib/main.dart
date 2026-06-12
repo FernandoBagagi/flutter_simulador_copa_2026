22f34a55dde8a279aa68ce069613279572822c6d
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-06-12 14:23:26 -0300

    configura a main pra injetar as dependencias, muda a cor primaria do aplicativo e adiciona dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_simulador_copa_2026/di/service_locator.dart';
 import 'package:flutter_simulador_copa_2026/views/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupInjecaoDependencias();
   runApp(const MyApp());
 }
 
@@ -13,8 +16,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simulador Copa 2026',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF004B23),
+          brightness: Brightness.light,
+        ),
       ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF004B23),
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: ThemeMode.system,
       home: const HomeScreen(),
     );
   }
