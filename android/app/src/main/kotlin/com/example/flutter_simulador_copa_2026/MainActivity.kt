5741cae97a6b9173090e1b0361ad6619febcf570
Fernando Bagagi <fernandosmartphone96@gmail.com>
2026-06-12 10:50:04 -0300

    cria o projeto

---
+++ android/app/src/main/kotlin/com/example/flutter_simulador_copa_2026/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_simulador_copa_2026
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simulador_copa_2026/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simulador Copa 2026',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
